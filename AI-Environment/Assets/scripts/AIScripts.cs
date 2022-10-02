3d5c8f4d294122d9f8ba7dc7a38be2688dd3aa3d
fakefarreraspol <79444349+fakefarreraspol@users.noreply.github.com>
2022-10-02 02:49:51 +0200

    w

---
+++ AI-Environment/Assets/scripts/AIScripts.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIScripts : MonoBehaviour
+{
+    
+    public GameObject target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
