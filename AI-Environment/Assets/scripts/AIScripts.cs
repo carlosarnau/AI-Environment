7ab754f2b6b6eadb60093368b65e71f2b50943b7
fakefarreraspol <79444349+fakefarreraspol@users.noreply.github.com>
2022-10-09 00:48:30 +0200

    w

--- AI-Environment/Assets/scripts/AIScripts.cs
+++ AI-Environment/Assets/scripts/AIScripts.cs
@@ -3,7 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.AI;
 using System.IO;
-
+using System.Linq;
+using System;
 
 public class AIScripts : MonoBehaviour
 {
@@ -19,21 +20,11 @@ public class AIScripts : MonoBehaviour
     public bool flee;
 
     //Stats
-    public float freq = 0f;
-    public float turnSpeed = 0.0f;
-    public float movSpeed = 0.0f;
-    public float acceleration = 0.1f;
-    public float turnAcceleration = 0.1f;
-    public float maxSpeed = 10.0f;
-    public float maxTurnSpeed = 2.0f;
     public float minDistance = 20.0f;
-    Quaternion rotation;
-    Vector3 movement;
     float stopDistance = 1.0f;
     float radius = 10.0f;
     float offset = 10.0f;
-    
-
+    GameObject[] hidingSpots;
     void Start()
     {
 
@@ -91,6 +82,17 @@ public class AIScripts : MonoBehaviour
 
     void Hide()
     {
+        Func<GameObject, float> distance =
+            (hs) => Vector3.Distance(target.transform.position,
+                                     hs.transform.position);
+        GameObject hidingSpot = hidingSpots.Select(
+            ho => (distance(ho), ho)
+            ).Min().Item2;
+        Vector3 dir = hidingSpot.transform.position - target.transform.position;
+        Ray backRay = new Ray(hidingSpot.transform.position, -dir.normalized);
+        RaycastHit info;
+        hidingSpot.GetComponent<Collider>().Raycast(backRay, out info, 50f);
+        Seek(info.point + dir.normalized);
 
 
 
---
+++ AI-Environment/Assets/scripts/FlockingManger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockingManger : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject[] allFish;
+    int numFish;
+    GameObject flockFish;
+    float spawnRadius;
+    void Start()
+    {
+        allFish = new GameObject[numFish];
+        for (int i = 0; i < numFish; ++i)
+        {
+            Vector3 randomPosition = new Vector3(Random.Range(-spawnRadius, spawnRadius), 0, Random.Range(-spawnRadius, spawnRadius)); //Random Position (Circle)
+            Vector3 pos = this.transform.position + randomPosition; 
+            Vector3 randomize = new Vector3(Random.Range(0, 360), 0, Random.Range(0, 360)); // random vector direction
+            
+            allFish[i] = (GameObject)Instantiate(flockFish, pos,
+                                Quaternion.LookRotation(randomize));
+            
+            //allFish[i].GetComponent<flockFish>().myManager = this;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
