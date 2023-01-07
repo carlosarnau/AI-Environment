80d1b9996656f7841769e508031b8e753bbb08dd
fakefarreraspol <79444349+fakefarreraspol@users.noreply.github.com>
2023-01-07 23:23:35 +0100

    change

--- AI-Environment/Assets/scripts/Slots.cs
+++ AI-Environment/Assets/scripts/Slots.cs
@@ -2,30 +2,30 @@ using UnityEngine;
 
 public class Slots : MonoBehaviour
 {
-    public int melee;
-    public GameObject meleePrefab;
-    public int missile;
-    public GameObject missilePrefab;
-    public int magic;
-    public GameObject magicPrefab;
-    public GameObject ghost;
+    public int fstRow;
+    public GameObject row01;
+    public int sndRow;
+    public GameObject row02;
+    public int trdrow;
+    public GameObject row03;
+    public GameObject king;
 
     void Start()
     {
-        int front = 2 * melee / 3;
-        int rear = melee - front;
-        createRow(front, -2f, meleePrefab);
-        createRow(missile, -4f, missilePrefab);
-        createRow(magic, -6f, magicPrefab);
-        createRow(rear, -8f, meleePrefab);
+        int front = 2 * fstRow / 3;
+        int rear = fstRow - front;
+        createRow(front, -2f, row01);
+        createRow(sndRow, -4f, row02);
+        createRow(trdrow, -6f, row03);
+        createRow(rear, -8f, row01);
     }
 
     void createRow(int num, float z, GameObject pf)
     {
         float pos = 1 - num;
         for (int i = 0; i < num; ++i) {
-            Vector3 position = ghost.transform.TransformPoint(new Vector3 (pos,0f,z));
-            GameObject temp = (GameObject)Instantiate(pf, position, ghost.transform.rotation); 
+            Vector3 position = king.transform.TransformPoint(new Vector3 (pos,0f,z));
+            GameObject temp = (GameObject)Instantiate(pf, position, king.transform.rotation); 
             //temp.AddComponent<Formation>();
             //temp.GetComponent<Formation>().pos = new Vector3 (pos,0,z);
             //temp.GetComponent<Formation>().agent = temp;
